396767f544342a40f133ed2034bc5abaf8ae54c3
scheids1 <rlscheidegger@gmail.com>
2022-06-14 09:09:26 -0400

    Updates

---
+++ precache-manifest.d4382c62964f77765ee414d2acfe272c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c446a16d7f893a1d2a811f4a2c7f7a57",
+    "url": "/poetry-prompt-generator/index.html"
+  },
+  {
+    "revision": "31eef31ab3cdd7374006",
+    "url": "/poetry-prompt-generator/static/css/main.28b1d41a.chunk.css"
+  },
+  {
+    "revision": "9aea1b67603180612303",
+    "url": "/poetry-prompt-generator/static/js/2.9c446683.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/poetry-prompt-generator/static/js/2.9c446683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31eef31ab3cdd7374006",
+    "url": "/poetry-prompt-generator/static/js/main.a95a5eb4.chunk.js"
+  },
+  {
+    "revision": "2c58030d1939d84545d7",
+    "url": "/poetry-prompt-generator/static/js/runtime-main.0056d918.js"
+  }
+]);
